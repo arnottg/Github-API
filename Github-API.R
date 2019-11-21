19bf76a2c1cdf02cb4cc0794584f35fe5b593a3e
Grant MacFarlane Arnott <arnottg@ITSERV.SCSS.TCD.IE>
2019-11-21 17:14:21 +0000

    Updated Template

--- Github-API.R
+++ Github-API.R
@@ -5,10 +5,8 @@ library(httpuv)
 #install.packages("httr")
 library(httr)
 
-# Can be github, linkedin etc depending on application
 oauth_endpoints("github")
 
-# Change based on what you 
 myapp <- oauth_app(appname = "Access_Github",
                    key = "322cc5e338050d04a076",
                    secret = "ad15687363f7c3bf5c0376fb0e2298c81db6528f")
@@ -18,16 +16,16 @@ github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
 # Use API
 gtoken <- config(token = github_token)
-req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+req <- GET("https://api.github.com/users/arnottg/repos", gtoken)
 
-# Take action on http error
+# Accounting for http error
 stop_for_status(req)
 
 # Extract content from a request
-json1 = content(req)
+extContent = content(req)
 
 # Convert to a data.frame
-gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(extContent))
 
 # Subset data.frame
-gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
+gitDF[gitDF$full_name == "arnottg/datasharing", "created_at"]
